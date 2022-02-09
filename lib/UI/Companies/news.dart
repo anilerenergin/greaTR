bcf974d68a25f9e320353f03e34e9336e72135c4
Anıl Eren Ergin <55100250+anilerenergin@users.noreply.github.com>
2022-02-09 12:40:39 +0300

    deneme

--- lib/UI/Companies/news.dart
+++ lib/UI/Companies/news.dart
@@ -1,7 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:greatr/UI/Companies/%20chip.dart';
 import 'package:greatr/UI/Companies/textWidget.dart';
 
 class SingleNews extends StatefulWidget {
